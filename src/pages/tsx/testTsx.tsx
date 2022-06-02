02b3b1e99b62277f92d3713cf277def9ec7263fd
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-02 10:22:13 +0800

    chore : update

--- src/pages/tsx/testTsx.tsx
+++ src/pages/tsx/testTsx.tsx
@@ -1,12 +1,12 @@
 export default defineComponent({
 
   setup() {
+    const count = ref(1)
 
-    
     return () => {
       return (
         <div>
-          <h1>Hello World</h1>
+          <h1>Hello World {count.value}</h1>
         </div>
       )
     }
