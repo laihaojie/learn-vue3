fee221791ee7d87fd89ad572f3ee60a1cc73a403
阿杰 <53169820+laihaojie@users.noreply.github.com>
2022-06-03 10:22:58 +0800

    feat: add auto router

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,27 @@ import '@unocss/reset/tailwind.css'
 import 'uno.css'
 
 const app = createApp(App)
+
+
+const modulesFiles = import.meta.globEager('./pages/template/*');
+
+const modules = Object.entries(modulesFiles).map(([path, mod]) => {
+  const regex = /^\.\/pages\/template\/(.*)\.vue$/;
+  const moduleName = path.replace(regex, '$1')
+  return {
+    path: `/${moduleName}`,
+    component: () => import(path)
+  }
+})
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: () => import('./pages/index.vue') },
     { path: '/tsx', component: () => import('./pages/tsx/testTsx') },
     { path: '/storage', component: () => import('./pages/storage/storage') },
-    { path: '/template', component: () => import('./pages/template/template.vue') },
+    ...modules,
   ],
 })
 app.config.errorHandler = (...ars) => {
